3f00f3a783186298d658c60e0e03c30d46e5c7fc
Senthilbsc <senthilvlr09@gmail.com>
2026-01-12 18:50:38 +0530

    added
    second commit update

--- js/Day1-Intro.js
+++ js/Day1-Intro.js
@@ -5,6 +5,7 @@
  */
 //1. fixed value (literals)
 console.log("Hello World")//string
+console.log("Hi JavaScript")//string
 console.log(12345)//number
 console.log(true)//boolean
 console.log([10,20, 'java', false])//array
